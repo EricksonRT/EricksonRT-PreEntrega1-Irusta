769753ad657cbfb089e45e83867e0f3b2da095d3
EricksonRT <erik_lauri@hotmail.com>
2022-11-02 19:12:26 -0300

    vbalidacion formulario

--- src/components/CartPage/componentsExtras/BuyCart.js
+++ src/components/CartPage/componentsExtras/BuyCart.js
@@ -8,34 +8,39 @@ const BuyCart = () => {
     phone: '',
     email: '',
   });
+  const [errors, setErrors] = useState('');
   const generarCompra = async (e) => {
     e.preventDefault();
-    const orden = {};
-    // Setea los valores del comprador
-    orden.buyer = {
-      name: formCompra.name,
-      phone: formCompra.phone,
-      email: formCompra.email,
-    };
+    if (checkInputs() == true) {
+      console.log(true);
+      const orden = {};
+      // Setea los valores del comprador
+      orden.buyer = {
+        name: formCompra.name,
+        phone: formCompra.phone,
+        email: formCompra.email,
+      };
 
-    // Setea el array con los productos comprados
-    orden.items = listCart.map((producto) => {
-      const id = producto.id;
-      const name = producto.nombre;
-      const price = producto.precio;
-      const quantity = producto.cant;
-      return { id, name, price, quantity };
-    });
-    // Setea el total de la compra
-    orden.total = precioTotal();
+      // Setea el array con los productos comprados
+      orden.items = listCart.map((producto) => {
+        const id = producto.id;
+        const name = producto.nombre;
+        const price = producto.precio;
+        const quantity = producto.cant;
+        return { id, name, price, quantity };
+      });
+      // Setea el total de la compra
+      orden.total = precioTotal();
 
-    // Crea el documento "orders" si no existe, y dentro crea un nuevo documento con los datos de la orden de compra
-    const db = getFirestore();
-    const orders = collection(db, 'orders');
-    addDoc(orders, orden) // setDoc(orders, obj, id)
-      .then((resp) => console.log('id de compra:' + resp))
-      .catch((err) => console.log(err))
-      .finally(() => cleanCart());
+      // Crea el documento "orders" si no existe, y dentro crea un nuevo documento con los datos de la orden de compra
+      //  // const orders = collection(db, 'orders');
+      // addDoc(orders, orden) // setDoc(orders, obj, id)
+      //   .then((resp) => console.log(resp))
+      //   .catch((err) => console.log(err));
+      // .finally(() => cleanCart());
+    } else {
+      console.log('Campos vacios');
+    }
   };
   // data= (e) de todos los imputs
   const valueImputChange = (data) => {
@@ -43,13 +48,34 @@ const BuyCart = () => {
       ...formCompra,
       [data.target.name]: data.target.value,
     });
-    // console.log([[formCompra);
+    // console.log(formCompra);
   };
-
+  // Valida que no estén vacío los campos
+  const checkInputs = () => {
+    if (
+      formCompra.name === '' ||
+      formCompra.phone === '' ||
+      formCompra.email === '' ||
+      formCompra.emailConfirm === ''
+    ) {
+      setErrors('Hay uno más campos vacios.');
+      return false;
+    } else if (formCompra.email !== formCompra.emailConfirm) {
+      setErrors('Los correos no coinciden');
+      return false;
+    }
+    errors !== '' && setErrors('');
+    return true;
+  };
   return (
     <>
       <div className="mt-2 col-7 d-flex mx-auto shadow p-3 mb-5 bg-body rounded mb-5 pb-5">
         <form className="row g-3">
+          {errors !== '' && (
+            <div class="mt-2 mb-2 alert alert-danger" role="alert">
+              {errors}
+            </div>
+          )}
           <div className="col-12">
             <label className="form-label">Nombre completo</label>
             <input
@@ -58,6 +84,7 @@ const BuyCart = () => {
               name="name"
               placeholder="ejemplo: Eric Laureano irusta"
               onChange={valueImputChange}
+              value={formCompra.name}
             />
           </div>
           <div className="col-md-6">
@@ -68,6 +95,7 @@ const BuyCart = () => {
               name="email"
               placeholder="usuario@gmail.com"
               onChange={valueImputChange}
+              value={formCompra.email}
             />
           </div>
           <div className="col-md-6">
@@ -80,6 +108,7 @@ const BuyCart = () => {
               name="emailConfirm"
               placeholder="usuario@gmail.com"
               onChange={valueImputChange}
+              value={formCompra.emailConfirm}
             />
           </div>
           <div className="col-md-6 mx-auto text-center">
@@ -90,6 +119,7 @@ const BuyCart = () => {
               name="phone"
               placeholder="xxxx-xxxxxx"
               onChange={valueImputChange}
+              value={formCompra.phone}
             />
           </div>
           <div className="col-12 text-center">
